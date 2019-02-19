5a7e8889a03811d314bb3fd7bd18127a45e7e4e6
Phong <ngo.minh.phong@framgia.com>
2019-02-19 10:03:20 +0700

    category: list

--- lib/uiza/category.rb
+++ lib/uiza/category.rb
@@ -2,12 +2,13 @@ module Uiza
   class Category
     extend Uiza::APIOperation::Create
     extend Uiza::APIOperation::Retrieve
+    extend Uiza::APIOperation::List
 
     OBJECT_API_PATH = "media/metadata".freeze
-
     OBJECT_API_DESCRIPTION_LINK = {
       create: "https://docs.uiza.io/#create-category",
-      retrieve: "https://docs.uiza.io/#retrieve-category"
+      retrieve: "https://docs.uiza.io/#retrieve-category",
+      list: "https://docs.uiza.io/#retrieve-category-list"
     }.freeze
   end
 end
